0220ba391a4fd9f8f32e24baf86393f32490df00
bhumiorange <bhumiorange@gmail.com>
2024-10-09 08:53:19 +0530

    Updated project

--- react-portfolio-bhumi/vite.config.js
+++ react-portfolio-bhumi/vite.config.js
@@ -4,10 +4,13 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  css:  {
+  css: {
     modules: {
       localsConvention: "camelCase"
     }
+  },
+  build: {
+    outDir: 'dist',
   }
-
 })
+
